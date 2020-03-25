9e1b8d5ad98d8f3f5dba7c4dbbf569262d300016
Takuya Hirata <takuya.hirata@dcmail.ca>
2020-03-25 18:03:44 -0400

    add testimonail mutations

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,6 +1,49 @@
 import uuid from 'uuid/v4'
 
 const Mutation = {
+  createTestimonial: async (parent, { ownerId, input }, { prisma }, info) => {
+    const owner = await prisma.exists.User({ id: ownerId })
+    if (!owner) throw new Error('Unable to find owner')
+
+    return prisma.mutation.createTestimonial(
+      { data: { ...input, owner: { connect: { id: ownerId } } } },
+      info
+    )
+  },
+  updateTestimonial: async (
+    parent,
+    { ownerId, testimonialId, input },
+    { prisma },
+    info
+  ) => {
+    const testimonialExists = await prisma.exists.Testimonial({
+      id: testimonialId,
+      owner: { id: ownerId }
+    })
+    if (!testimonialExists) throw new Error('Testimonial not found')
+
+    return prisma.mutation.updateTestimonial(
+      { data: input, where: { id: testimonialId } },
+      info
+    )
+  },
+  deleteTestimonial: async (
+    parent,
+    { ownerId, testimonialId },
+    { prisma },
+    info
+  ) => {
+    const testimonialExists = await prisma.exists.Testimonial({
+      id: testimonialId,
+      owner: { id: ownerId }
+    })
+    if (!testimonialExists) throw new Error('Testimonial not found')
+
+    return prisma.mutation.deleteTestimonial(
+      { where: { id: testimonialId } },
+      info
+    )
+  },
   createProject: async (parent, { input, ownerId }, { prisma }, info) => {
     const owner = await prisma.exists.User({ id: ownerId })
     if (!owner) throw new Error('Unable to find owner')
